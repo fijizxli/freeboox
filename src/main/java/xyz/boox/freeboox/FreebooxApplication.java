7c7e06487713b5001769f0f118ae7ff9ffa615f0
Zoltán Szabó <szabozoltan6x@gmail.com>
2023-09-06 20:23:48 +0200

    Initial commit.

---
+++ src/main/java/xyz/boox/freeboox/FreebooxApplication.java
@@ -0,0 +1,13 @@
+package xyz.boox.freeboox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FreebooxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FreebooxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/xyz/boox/freeboox/FreebooxApplicationTests.java
@@ -0,0 +1,13 @@
+package xyz.boox.freeboox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FreebooxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
