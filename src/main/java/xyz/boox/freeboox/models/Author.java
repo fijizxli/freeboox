f42b8a3cb11806ada704e64ca692e74c9ad71c17
Zoltán Szabó <szabozoltan6x@gmail.com>
2023-09-07 07:11:44 +0200

    Created Author class.

---
+++ src/main/java/xyz/boox/freeboox/models/Author.java
@@ -0,0 +1,73 @@
+package xyz.boox.freeboox.models;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import java.util.Date;
+import java.util.List;
+
+@Entity
+public class Author {
+    @Id
+    private Integer id;
+    private String name;
+    private Date dateOfBirth;
+    private Boolean isAlive;
+
+    @OneToMany
+    private List<Book> books;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String title) {
+        this.name = title;
+    }
+
+    public Date getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(Date dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public Boolean getAlive() {
+        return isAlive;
+    }
+
+    public void setAlive(Boolean alive) {
+        isAlive = alive;
+    }
+
+    public Author(Integer id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Author(Integer id, String name, List<Book> books) {
+        this.id = id;
+        this.name = name;
+        this.books = books;
+    }
+
+    public Author(Integer id, String name, Date dateOfBirth, Boolean isAlive, List<Book> books) {
+        this.id = id;
+        this.name = name;
+        this.dateOfBirth = dateOfBirth;
+        this.isAlive = isAlive;
+        this.books = books;
+    }
+
+    public Author() {
+    }
+}
