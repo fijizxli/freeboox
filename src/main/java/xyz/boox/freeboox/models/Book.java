3a4e6931bda850f12b0f22e811a7e46b9ebc7197
Zoltán Szabó <szabozoltan6x@gmail.com>
2023-09-07 07:11:53 +0200

    Created Book class.

---
+++ src/main/java/xyz/boox/freeboox/models/Book.java
@@ -0,0 +1,77 @@
+package xyz.boox.freeboox.models;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+@Entity
+public class Book {
+
+    @Id
+    private Long id;
+    private String title;
+    private String description;
+    @ManyToOne
+    private Author author;
+    private Integer numberOfPages;
+
+    //private Image cover;
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Integer getNumberOfPages() {
+        return numberOfPages;
+    }
+
+    public void setNumberOfPages(Integer numberOfPages) {
+        this.numberOfPages = numberOfPages;
+    }
+
+    public Author getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(Author author) {
+        this.author = author;
+    }
+
+    public Book(Long id, String title, String description, Integer rating, Author author, Integer numberOfPages) {
+        this.id = id;
+        this.title = title;
+        this.description = description;
+        this.author = author;
+        this.numberOfPages = numberOfPages;
+    }
+
+    public Book(Long id, String title, Author author) {
+        this.id = id;
+        this.title = title;
+        this.author = author;
+    }
+
+    public Book() {
+    }
+}
