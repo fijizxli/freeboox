ca1942778e2588847fa262a7672bef5c7447f816
Zoltán Szabó <szabozoltan6x@gmail.com>
2023-09-21 08:59:42 +0200

    Update HomeController

--- src/main/java/xyz/boox/freeboox/controller/HomeController.java
+++ src/main/java/xyz/boox/freeboox/controller/HomeController.java
@@ -1,12 +1,23 @@
 package xyz.boox.freeboox.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import xyz.boox.freeboox.models.Book;
+import xyz.boox.freeboox.repositories.BookRepository;
+
+import java.util.List;
 
 @Controller
 public class HomeController {
-    @GetMapping("")
-    public String home() {
+    @Autowired
+    BookRepository br;
+    @GetMapping("/")
+    public String home(Model model) {
+        List<Book> books = br.findAll();
+        model.addAttribute("books", books);
+
         return "home";
     }
 }
